c3a9664f2ffe79446d15245b0bd0603ba3486305
jinwen <421986908@qq.com>
2015-12-29 02:54:16 +0800

    version 0.3

--- main.c
+++ main.c
@@ -12,7 +12,8 @@ struct threadpool *gThreadPool;
 
 int get_listen_socket()
 {
-	int sock;
+	int sock,res;
+	int reuse = 1;
 	struct sockaddr_in addr;
 	sock = socket(AF_INET,SOCK_STREAM,0);
 
@@ -22,19 +23,52 @@ int get_listen_socket()
 		exit(0);
 	}
 
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	bzero(&addr, sizeof(addr));
+	addr.sin_addr.s_addr = inet_addr("0.0.0.0");
 	addr.sin_port = htons(51005);
 	addr.sin_family = AF_INET;
 
-	bind(sock,(struct sockaddr *)&addr,sizeof(struct sockaddr));
-	listen(sock,10);
+	res = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+	if(res)
+	{
+		perror("setsockopt error!");
+		exit(0);
+	}
+
+	res = bind(sock,(struct sockaddr *)&addr,sizeof(struct sockaddr));
+	if(res)
+	{
+		perror("bind error!");
+		exit(0);
+	}
+
+	res = listen(sock,10);
+	if(res)
+	{
+		perror("listen error!");
+		exit(0);
+	}
 
 	return sock;
 }
 
 void do_job(client)
 {
-	char buf[20] = "hello!";
+	char buf[1024];
+	int len;
+//#if 0
+	while(1)
+	{
+		len = read(client,buf,sizeof(buf));
+		if(!memcmp("exit",buf,len))
+		{
+			send(client,"GoodBye",7,0);
+			close(client);
+			return;
+		}
+		send(client,buf,len,0);
+	}
+//#endif
 	send(client,buf,strlen(buf),0);
 	close(client);
 }
@@ -47,6 +81,11 @@ void *work(void *arg)
 	socklen_t len;
 	
 	client = accept(*listensock,(struct sockaddr *)&remote,&len);
+	if(client < 0)
+	{
+		perror("accept error!");
+		return;
+	}
 
 	threadpool_add_job( gThreadPool, work, (void *)listensock );
 
